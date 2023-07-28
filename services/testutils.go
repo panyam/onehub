ca90e3aeea6dd5199dcabf3dee4422c97cebb5ac
Dag Meister <sri@dagknows.com>
2023-07-28 00:38:38 -0700

    Adding sample tests

---
+++ services/testutils.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+	"log"
+	"net"
+	"testing"
+)
+
+type DialerFunc = func(context.Context, string) (net.Conn, error)
+type ServerInitFunc = func(server *grpc.Server)
+type TestBodyFunc = func(ctx context.Context, conn *grpc.ClientConn)
+
+func TestDialer() (*grpc.Server, DialerFunc) {
+	listener := bufconn.Listen(1024 * 1024)
+	server := grpc.NewServer()
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	return server, func(context.Context, string) (net.Conn, error) {
+		return listener.Dial()
+	}
+}
+
+func RunTest(t *testing.T, init_server ServerInitFunc, test_body TestBodyFunc) {
+	ctx := context.Background()
+	server, dialer_func := TestDialer()
+	init_server(server)
+	conn, err := grpc.DialContext(ctx, "", grpc.WithInsecure(), grpc.WithContextDialer(dialer_func))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	test_body(ctx, conn)
+}
---
+++ services/topics_test.go
@@ -0,0 +1,50 @@
+package services
+
+import (
+	"context"
+	"testing"
+
+	protos "github.com/panyam/onehub/gen/go/onehub/v1"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	_ "google.golang.org/grpc/codes"
+	_ "google.golang.org/grpc/status"
+)
+
+func TestCreateTopicAndFetch(t *testing.T) {
+	svc := NewTopicService(nil)
+	RunTest(t,
+		func(server *grpc.Server) {
+			protos.RegisterTopicServiceServer(server, svc)
+		},
+		func(ctx context.Context, conn *grpc.ClientConn) {
+			client := protos.NewTopicServiceClient(conn)
+			resp, err := client.CreateTopic(ctx, &protos.CreateTopicRequest{
+				Topic: &protos.Topic{
+					Name:  "First Topic",
+					Users: []string{"1", "2", "3"},
+				},
+			})
+			assert.Equal(t, err, nil, "Error should be nil")
+			assert.Equal(t, resp.Topic.Id, "1")
+			assert.Equal(t, resp.Topic.Name, "First Topic")
+			assert.Equal(t, resp.Topic.Users, []string{"1", "2", "3"})
+
+			// Create another
+			resp, err = client.CreateTopic(ctx, &protos.CreateTopicRequest{
+				Topic: &protos.Topic{
+					Name:  "An awesome second song",
+					Users: []string{"4", "2", "5"},
+				},
+			})
+			assert.Equal(t, err, nil, "Error should be nil")
+			assert.Equal(t, resp.Topic.Id, "2")
+			assert.Equal(t, resp.Topic.Name, "An awesome second song")
+			assert.Equal(t, resp.Topic.Users, []string{"4", "2", "5"})
+
+			resp2, err := client.GetTopics(ctx, &protos.GetTopicsRequest{
+				Ids: []string{"1", "2"},
+			})
+			assert.Equal(t, len(resp2.Topics), 2)
+		})
+}
