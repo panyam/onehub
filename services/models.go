5e006a67c5db1da3b58a3ad5d2368b68037c4fd7
Dag Meister <sri@dagknows.com>
2023-07-28 17:32:36 -0700

    Adding gorm models

---
+++ services/models.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type BaseModel struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Id        string `gorm:"primaryKey"`
+	Version   int    // used for optimistic locking
+}
+
+type Topic struct {
+	BaseModel
+	CreatorId string
+	Name      string         `gorm:"index:SortedByName"`
+	Users     pq.StringArray `gorm:"type:text[]"`
+}
+
+type Message struct {
+	BaseModel
+
+	ParentId    string
+	TopicId     string    `gorm:"index:SortedByTopicAndCreation,priority:1"`
+	CreatedAt   time.Time `gorm:"index:SortedByTopicAndCreation,priority:2"`
+	SourceId    string
+	UserId      string
+	ContentType string
+	ContentText string
+	ContentData map[string]interface{} `gorm:"type:json"`
+}
