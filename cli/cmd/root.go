dbab4f86dbc06679cf6cdba7aa508996c231e8c2
Dag Meister <sri@dagknows.com>
2023-08-08 13:42:53 -0400

    Moving to a functional approach to declaring commands

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -15,17 +15,24 @@ var username = ""
 var password = ""
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "oh",
-	Short: "The OneHub CLI",
-	Long:  `The CLI for interacting with OneHub in a simpler but more flexible way`,
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		if username == "" || password == "" {
-			return errors.New(fmt.Sprintf("Invalid username '%s' or password '%s'", username, password))
-		}
-		return nil
-	},
+func rootCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "oh",
+		Short: "The OneHub CLI",
+		Long:  `The CLI for interacting with OneHub in a simpler but more flexible way`,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if username == "" || password == "" {
+				return errors.New(fmt.Sprintf("Invalid username '%s' or password '%s'", username, password))
+			}
+			return nil
+		},
+	}
+	out.PersistentFlags().StringVar(&username, "username", "", "Username to use for basic auth for all commands")
+	out.PersistentFlags().StringVar(&password, "password", "", "Password to use for basic auth for all commands")
+	return out
 }
+
+var rootCmd = rootCommand()
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -34,9 +41,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	rootCmd.PersistentFlags().StringVar(&username, "username", "", "Username to use for basic auth for all commands")
-	rootCmd.PersistentFlags().StringVar(&password, "password", "", "Password to use for basic auth for all commands")
 }
--- cli/cmd/topics.go
+++ cli/cmd/topics.go
@@ -9,45 +9,77 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// topicsCmd represents the topics command
-var topicsCmd = &cobra.Command{
-	Use:   "topics",
-	Short: "Manage topics",
-	Long:  `Group of commands to manage and interact with topic`,
+// topicsCmd represents the entire topics command hierarchy
+func topicsCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "topics",
+		Short: "Manage topics",
+		Long:  `Group of commands to manage and interact with topic`,
+	}
+
+	out.AddCommand(getCommand())
+	out.AddCommand(listCommand())
+	out.AddCommand(deleteCommand())
+	return out
 }
 
-var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List topics",
-	Long:  `List topics in the system optionally filtered by name`,
-	Run: func(cmd *cobra.Command, args []string) {
-		name, _ := cmd.Flags().GetString("name")
-		if name != "" {
-			fmt.Println("Listing topics by name: ", name)
-		} else {
-			fmt.Println("Listing all topics")
-		}
-	},
+func listCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "list",
+		Short: "List topics",
+		Long:  `List topics in the system optionally filtered by name`,
+		Run: func(cmd *cobra.Command, args []string) {
+			name, _ := cmd.Flags().GetString("name")
+			if name != "" {
+				fmt.Println("Listing topics by name: ", name)
+			} else {
+				fmt.Println("Listing all topics")
+			}
+		},
+	}
+	out.Flags().StringP("name", "n", "", "Match topics with name")
+	return out
 }
 
-var getCmd = &cobra.Command{
-	Use:   "get",
-	Short: "Get topics",
-	Long:  `Get one or more topics by ID (or list of IDs`,
-	Run: func(cmd *cobra.Command, args []string) {
-		for _, topicid := range args {
-			fmt.Println("Getting topic by ID: ", topicid)
-		}
-	},
+func getCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "get",
+		Short: "Get topics",
+		Long:  `Get one or more topics by ID (or list of IDs`,
+		Run: func(cmd *cobra.Command, args []string) {
+			for _, topicid := range args {
+				fmt.Println("Getting topic by ID: ", topicid)
+			}
+		},
+	}
+}
+
+func deleteCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "delete",
+		Short: "Delete topics",
+		Long:  `Delete one or more topics by ID (or list of IDs`,
+		Run: func(cmd *cobra.Command, args []string) {
+			for _, topicid := range args {
+				fmt.Println("Deleting topic by ID: ", topicid)
+			}
+		},
+	}
+}
+
+func createCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "new",
+		Short: "Create a new",
+		Long:  `Delete one or more topics by ID (or list of IDs`,
+		Run: func(cmd *cobra.Command, args []string) {
+			for _, topicid := range args {
+				fmt.Println("Deleting topic by ID: ", topicid)
+			}
+		},
+	}
 }
 
 func init() {
-	rootCmd.AddCommand(topicsCmd)
-
-	// Add the get command to our topics group
-	topicsCmd.AddCommand(getCmd)
-
-	// Add the list command to our topics group
-	topicsCmd.AddCommand(listCmd)
-	listCmd.Flags().StringP("name", "n", "", "Match topics with name")
+	rootCmd.AddCommand(topicsCommand())
 }
