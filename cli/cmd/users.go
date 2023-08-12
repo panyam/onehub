cd7a3692329615d6b02f0284f4cfebbd7940e974
Dag Meister <sri@dagknows.com>
2023-08-12 13:25:31 -0400

    Adding users endpoint

--- cli/cmd/users.go
+++ cli/cmd/users.go
@@ -1,40 +1,119 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
-// usersCmd represents the users command
-var usersCmd = &cobra.Command{
-	Use:   "users",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+func usersCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "users",
+		Short: "Manage users",
+		Long:  `Commands to add/remove/list users in OneHub`,
+	}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("users called")
-	},
+	out.AddCommand(getUsersCommand())
+	out.AddCommand(listUsersCommand())
+	out.AddCommand(deleteUsersCommand())
+	out.AddCommand(newUserCommand())
+	// out.AddCommand(updateUserCommand())
+	return out
+}
+
+func newUserCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "new",
+		Short: "Create a new user",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			name, _ := cmd.Flags().GetString("name")
+			if name == "" {
+				return errors.New("User's name must be specified")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			id, _ := cmd.Flags().GetString("id")
+			name, _ := cmd.Flags().GetString("name")
+			avatar, _ := cmd.Flags().GetString("avatar")
+			payload := StringMap{
+				"user": StringMap{
+					"id":     id,
+					"name":   name,
+					"avatar": avatar,
+				},
+			}
+			path := "/v1/users"
+			Client.Call("POST", path, nil, nil, payload)
+		},
+	}
+	out.Flags().StringP("id", "i", "", "A custom ID to use instead of auto generating one")
+	out.Flags().StringP("type", "t", "text", "Content type to assign to the content")
+	out.Flags().StringP("file", "f", "", "Load user content from the given file if user not passed as a command line arg")
+	out.Flags().StringP("data", "d", "", "Extra JSON data to save as part of the content")
+	return out
+}
+
+func listUsersCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "list TOPIC_ID",
+		Short: "List users",
+		Long:  `List users in a topic`,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errors.New("A TOPIC_ID must be provided")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			path := fmt.Sprintf("/v1/topics/%s/users", args[0])
+			Client.Call("GET", path, nil, nil, nil)
+		},
+	}
+	return out
+}
+
+func getUsersCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "get MSG_ID [...MSG_IDS]",
+		Short: "Get a user for one or more user IDs",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("At least one user ID must be provided")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			for _, mid := range args {
+				path := fmt.Sprintf("/v1/users/%s", mid)
+				Client.Call("GET", path, nil, nil, nil)
+			}
+		},
+	}
+	return out
+}
+
+func deleteUsersCommand() *cobra.Command {
+	out := &cobra.Command{
+		Use:   "delete MSG_ID [...MSG_IDS]",
+		Short: "Delete a user by one or more user IDs",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("At least one user ID must be provided")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			for _, mid := range args {
+				path := fmt.Sprintf("/v1/users/%s", mid)
+				Client.Call("DELETE", path, nil, nil, nil)
+			}
+		},
+	}
+	return out
 }
 
 func init() {
-	rootCmd.AddCommand(usersCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// usersCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// usersCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(usersCommand())
 }
